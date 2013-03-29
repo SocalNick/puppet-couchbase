dff2777e51fbb071ab4f2b7577d6026d885efe2a
Nicholas Calugar <njcalugar@gmail.com>
2013-03-29 11:48:05 -0700

    Adding init puppet script

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# Public: Installs the Couchbase Database app
+#
+# Example
+#
+#   include couchbase
+
+class couchbase {
+  package { 'Couchbase':
+    provider => 'compressed_app',
+    source   => 'http://packages.couchbase.com/releases/2.0.0/couchbase-server-community_x86_64_2.0.0.zip'
+  }
+}
