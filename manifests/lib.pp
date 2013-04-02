0e4fc16957aaaf90dcbda98e8850c33f30c9f337
Nicholas Calugar <njcalugar@gmail.com>
2013-04-01 17:19:31 -0700

    Fixing couchbase::lib class

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -1,15 +1,8 @@
 class couchbase::lib {
-  
-  include homebrew
 
-  homebrew::formula {
-    'libcouchbase': 
-      before => Package['boxen/brews/libcouchbase'] ;
-  }
-
-  package {
-    'boxen/brews/libcouchbase':
-      ensure => '1.4.13-boxen1'
+  package { "libcouchbase":
+    provider => "homebrew",
+    ensure => present,
   }
 
 }
